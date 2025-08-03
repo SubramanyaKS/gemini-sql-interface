67ba810768387cc2ffb658e3b678fcc495998ea1
SubramanyaKS <subramanyaks2205@gmail.com>
2025-08-03 19:10:57 +0530

    small modification

--- app.py
+++ app.py
@@ -2,26 +2,50 @@ from services.llm import geminiCall
 import streamlit as st
 from services.schema import extract_schema
 from services.connect import get_connection
-from utils.constant import title,connect_success,connect_failure
-st.title(title)
+from utils.constant import title,connect_success,connect_failure,subtitle,caption
 
+
+st.title(title)
+st.subheader(subtitle)
+st.caption(caption)
 db_url=st.sidebar.text_input("Enter postgres database url?", type="password")
 
+
 if db_url:
-    st.warning("Connecting Database")
     conn = get_connection(db_url)
+    if not conn:
+        st.warning("Connecting Database")
     
     if conn:
         st.success(connect_success)
         res= extract_schema(conn)
+        st.write("Database Schema")
+        st.code(res)
         api_instruct ="""You are a PosgreSQL expert. Convert natural language questions into accurate, efficient posgreSQL queries. Use standard SQL syntax. the table schema is provided ${res}.Do not include DROP, DELETE, UPDATE, INSERT, or any data-modifying command."""
+        st.subheader("Curious? Ask away!")
         with st.form("text_to_sql_form"):
             user_input = st.text_input("Enter your question in natural language:")
             submitted = st.form_submit_button("Generate SQL")
         if submitted:
             result_sql=geminiCall(user_input,api_instruct)
+            st.session_state["generated_sql"] = result_sql
+            
+        if "generated_sql" in st.session_state:
             st.subheader("Generated SQL Query")
-            st.code(result_sql,language='sql')
+            st.write(st.session_state["generated_sql"])
+            print(st.session_state["generated_sql"])
+
+            if st.button("Execute the SQL"):
+                raw_sql = st.session_state["generated_sql"]
+                clean_sql = raw_sql.strip().replace("```sql", "").replace("```", "").strip()
+                cursor = conn.cursor()
+                try:
+                    cursor.execute(clean_sql)
+                    query_result = cursor.fetchall()
+                    st.write(query_result)
+                except Exception as e:
+                    st.error(f"Error executing query: {e}")
+
     else:
         st.error(connect_failure)
 
--- utils/constant.py
+++ utils/constant.py
@@ -1,3 +1,5 @@
 title = "Text-to-SQL Explorer"
 connect_success="Connection to the PostgreSQL established successfully."
 connect_failure="Connection to the PostgreSQL encountered and error."
+subtitle="🔌 Connect to Your PostgreSQL Database"
+caption="Securely enter your database URL to begin querying."
